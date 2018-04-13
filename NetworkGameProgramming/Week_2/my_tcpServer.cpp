a1c5790e7425aa59f62ae4ad854030a283b2a12f
onemoon <onemoonstudio@gmail.com>
2018-04-13 16:16:22 +0900

    feat : week2 tcp code
    tcp ip client and server code is added

--- NetworkGameProgramming/Week_2/my_tcpServer.cpp
+++ NetworkGameProgramming/Week_2/my_tcpServer.cpp
@@ -1,3 +1,7 @@
+/*
+	TCP 방식으로 데이터를 주고 받기 위해 구현한 서버	
+*/
+
 #include <stdio.h>
 #include <WinSock2.h>
 
@@ -7,12 +11,13 @@ void main()
 	SOCKET socketListen, socketClient;
 	struct sockaddr_in serverAddr;
 
-	// 네트워크 초기화
 	::WSAStartup( 0x202, &wsaData );
+	// 2.2 버전을 사용한다.
 
 
 	socketClient = INVALID_SOCKET;
 	socketListen = ::socket( AF_INET, SOCK_STREAM, IPPROTO_TCP );
+	// 디스크립터 할당 ipv4, stream , tcp
 	if( socketListen == INVALID_SOCKET )
 	{
 		printf( "Socket create error !!\n" );
@@ -23,18 +28,24 @@ void main()
 	serverAddr.sin_family		= AF_INET;
 	serverAddr.sin_addr.s_addr	= ::htonl( INADDR_ANY ); // ::inet_addr( "165.194.115.25" ); //::htonl( INADDR_LOOPBACK  );
 	serverAddr.sin_port			= ::htons( 8600 );
+	// 서버 구조체 초기화 
+	// ipv4 , 어디서든 들어올 수 있도록 설정 , 8600 포트
 
 	if( ::bind( socketListen, ( struct sockaddr* )&serverAddr, sizeof( serverAddr ) ) == SOCKET_ERROR )
 	{
 		printf( "bind failed!! : %d\n", ::WSAGetLastError() );
 		return;
 	}
+	// 디스크립터에 구조체를 설정해준다.
+	// 디스크립터 , 구조체의 주소 , 구조체의 사이즈 
 
 	if( ::listen( socketListen, SOMAXCONN ) == SOCKET_ERROR )
 	{
 		printf( "listen failed!! : %d\n", ::WSAGetLastError() );
 		return;
 	}
+	// connect가 들어올때 까지 대기한다
+	// 총 SOMAXCONN 까지 연결을 받을 수 있다. ( 대기 큐 )
 
 	while( 1 )
 	{
@@ -46,15 +57,20 @@ void main()
 
 			FD_ZERO( &fds );
 			FD_SET( socketListen, &fds );
+			// socketlisten 을 fds에 할당
 
 			::select( 0, &fds, 0, 0, &tv );
 			if( FD_ISSET( socketListen, &fds ) )
+			// 정상적으로 socketListen이 할당되었을 경우
 			{
 				struct sockaddr_in fromAddr;
 				int size = sizeof( fromAddr );
 
 				socketClient = ::accept( socketListen, ( struct sockaddr* )&fromAddr, &size );
+				// accept를 통해서 socketClient를 만든다. 
+				// client에서는 connect와 연결된다. 
 				printf( "Accepted a client : %s\n", ::inet_ntoa( fromAddr.sin_addr ) );
+				// 클라이언트의 주소를 보여준다.
 			}
 		}
 		else
@@ -62,16 +78,18 @@ void main()
 			// 접속이 이루어진 후에는 echo 통신을 한다.
 			while(1){
 
-			char recvBuffer[127];
+			char recvBuffer[128];
 			int recvBytes;
 
-			recvBytes = ::recv( socketClient, recvBuffer, 127, 0); //MSG_OOB );
+			recvBytes = ::recv( socketClient, recvBuffer, 128, 0); //MSG_OOB );
+			// 128 바이트를 서버로부터 받는다.
 			if(recvBytes<2)
 				break;
 
 			printf( "%d bytes received : %s\n", recvBytes, recvBuffer );
 
 			::send( socketClient, recvBuffer, recvBytes, 0 );
+			// echo
 			}
 
 			// 통신이 끝난 후에는 클라이언트 접속을 해제한다.
--- NetworkGameProgramming/Week_2/my_tcpclient.cpp
+++ NetworkGameProgramming/Week_2/my_tcpclient.cpp
@@ -1,3 +1,6 @@
+/*
+	TCP 방식으로 데이터를 주고 받기 위해 구현한 클라이언트
+*/
 #include <stdio.h>
 #include <WinSock2.h>
 
@@ -7,8 +10,10 @@ void main()
 	SOCKET socketConnect;
 	struct sockaddr_in serverAddr;
 	::WSAStartup( 0x202, &wsaData );
+	// 2.2 버전을 사용한다.
 
 	socketConnect = ::socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	// 디스크립터 할당 ipv4, stream , tcp
 	if (socketConnect == INVALID_SOCKET)
 		printf("socket() error!");
 
@@ -17,16 +22,20 @@ void main()
 	serverAddr.sin_family = AF_INET;
 	serverAddr.sin_addr.s_addr = ::inet_addr("127.0.0.1");
 	serverAddr.sin_port = ::htons(8600);
+	// 서버 구조체 초기화 
+	// ipv4 , address , port
 		
 
 	if (::connect(socketConnect, (struct sockaddr*)&serverAddr, sizeof(serverAddr) ) == SOCKET_ERROR)
 		printf("connect() error!");
+		// server로 connect 
+		// listen 상태여야 한다. 아니면 error
 
 	
 	// 서버와 통신을 한다. 
 	while(1){
 		
-		char sendBuffer[128];
+		char sendBuffer[127];
 		char recvBuffer[128];
 		int sentBytes, recvBytes;
 
@@ -38,15 +47,18 @@ void main()
 		fputc('\n', stderr);
 
 
-		sentBytes = ::send(socketConnect,sendBuffer,sizeof(sendBuffer)-1, 0);
+		sentBytes = ::send(socketConnect,sendBuffer,sizeof(sendBuffer)+1, 0);
 		printf( "%d bytes sent.\n", sentBytes );
+		// +1 을 붙이는 이유는 NULL이 포함될 수 있기 때문에 붙인다.
 
-		recvBytes = ::recv(socketConnect, recvBuffer, sizeof(recvBuffer)-1, 0);
+		recvBytes = ::recv(socketConnect, recvBuffer, sizeof(recvBuffer), 0);
+		// recv는 그대로 받는다.
 		printf( "%d bytes Received: %s\n", recvBytes, recvBuffer );
 	}
 
 	::shutdown( socketConnect, SD_BOTH );
 	::closesocket( socketConnect );
 	::WSACleanup();
+	// socket 을 종료한다.
 	system("pause");
 }
